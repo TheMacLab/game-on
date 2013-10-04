925bf97b7fb43154bc668ca8d907207df003372b
Semar Yousif <semarpetrus@yahoo.com>
2013-10-04 08:48:56 -0700

    Beta update and fixing bugs
    Fixed a problem with classifications.
    Added a remove option for classifications.
    Tasks no longer need to be on a page or post (works for most themes,
    working on the others).
    Removed Parents (hierarchical) from tasks.
    Added Tags to tasks.

--- types/types.php
+++ types/types.php
@@ -191,4 +191,33 @@ function go_cmb_render_go_shortcode() {
  echo '<input type="text" value="[go_task id=\''.get_the_id().'\']"';
  
 }
-?>
+
+
+
+add_filter( 'template_include', 'go_tasks_template_function', 1 );
+
+function go_tasks_template_function( $template_path ) {
+    if ( get_post_type() == 'tasks' ) {
+        if ( is_single() ) {
+            // checks if the file exists in the theme first,
+            // otherwise serve the file from the plugin
+			
+			
+			
+            if ( $theme_file = locate_template( array (  'index.php') )
+		//$theme_file =	get_page_template()
+		 ) {
+                $template_path = $theme_file;
+				add_filter( 'the_content', 'go_tasks_filter_content' );
+            } 
+        }
+    }
+    return $template_path;
+}
+
+function go_tasks_filter_content(){
+	 global $wpdb;
+	 echo do_shortcode('[go_task id="'.get_the_id().'"]');
+	 }
+
+
