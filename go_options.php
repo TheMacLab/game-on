745c8ec8434e7a481064ed8270afd0248585e047
Ezio Ballarin <ezio.t.ballarin@gmail.com>
2014-01-18 13:38:50 -0800

    1.2.1
    Added videos to the options page
    Minor tweaks to options page

--- go_options.php
+++ go_options.php
@@ -1,7 +1,8 @@
 <?php
 if (is_admin()) {
-function go_opt_help($field, $title) {
-	echo '<a id="go_help_'.$field.'" class="go_opt_help" onclick="" title="'.$title.'">?</a>';
+function go_opt_help($field, $title, $video_url = null) {
+	echo '<a id="go_help_'.$field.'" class="go_opt_help" onclick="go_display_help_video(\''.$video_url.'\');" title="'.$title.'">?</a>';
+	
 }
 function go_opt_style() {
     wp_register_style( 'go_opt_css', plugins_url( 'css/options.css' , __FILE__ ), false, '1.0.0' );
@@ -9,9 +10,9 @@ function go_opt_style() {
 }
 add_action('admin_enqueue_scripts', 'go_opt_style');
 
-function go_sub_option($explanation_name, $explanation, $title, $field_name, $option_name, $explanation_question){ ?>
+function go_sub_option($explanation_name, $explanation, $title, $field_name, $option_name, $explanation_question, $video_url = null){ ?>
 	    <div class="pa">
-            	<?php go_opt_help($explanation_name,$explanation); ?> 
+            	<?php go_opt_help($explanation_name,$explanation, $video_url); ?> 
             	<strong><?php echo $title; ?>:</strong><br />  
                 <input type="text" name="<?php echo $field_name; ?>" size="45" value="<?php echo get_option($option_name); ?>" /><br />
                 <i><?php echo $explanation_question; ?></i> 
@@ -42,41 +43,47 @@ window.location = "'.admin_url().'/?page=game-on-options.php"
 </script>';
 	}
 ?>  
+    <div class="dark"> </div>
+    <div class="light">
+        <div style="margin: 10px 10px 10px 10px; width: 864px; height: 540px;">
+        	<video id="go_option_help_video" class="video-js vjs-default-skin vjs-big-play-centered" controls height="100%" width="100%" ><source src="" type="video/mp4"/></video>
+        </div>
+    </div>
     <div class="go-wrap">  
-        <h2>Game On Options</h2>  
+        <h2>Game On Options </h2><?php echo 'Click ?\'s for videos.'; ?>
         <form method="post" action="options.php">  
             <?php wp_nonce_field('update-options') ?> 
             <div id="tsk" class="opt-box">
             <h3>Task Settings</h3>
             <?php
-			echo	go_sub_option( 'tasks_name', 'This is the word that will be used in place of Task all over your website. Make sure that it is singular (e.g. Assignment, Quest)', 'Singular Tasks Name', 'go_tasks_name', 'go_tasks_name', 'what would you like tasks to be called? (singular)' );
-			echo	go_sub_option('tasks_plural_name','This is the word used in place of Task all over your website. Use only plural words here (e.g. Assignments, Quests)','Plural Tasks Name', 'go_tasks_plural_name', 'go_tasks_plural_name', 'what would you like tasks to be called? (plural)' );
+			echo	go_sub_option( 'tasks_name', 'This is the word that will be used in place of Task all over your website. Make sure that it is singular (e.g. Assignment, Quest)', 'Singular Tasks Name', 'go_tasks_name', 'go_tasks_name', 'what would you like tasks to be called? (singular)', 'http://maclab.guhsd.net/go/video/questNames.m4v');
+			echo	go_sub_option('tasks_plural_name','This is the word used in place of Task all over your website. Use only plural words here (e.g. Assignments, Quests)','Plural Tasks Name', 'go_tasks_plural_name', 'go_tasks_plural_name', 'what would you like tasks to be called? (plural)', 'http://maclab.guhsd.net/go/video/questNames.m4v');
 		
-			echo	go_sub_option( 'first_stage_name', 'This is the word that will be used for the first stage of the Task stages which is triggered upon visting the page for the first time. Such as Encountered.', 'First Stage Name', 'go_first_stage_name', 'go_first_stage_name', 'What would you like the first stage to be called?'  );
-			echo	go_sub_option( 'second_stage_name', 'This is the word that will be used for the second stage of Task stages. Such as Accepted.', 'Second Stage Name', 'go_second_stage_name','go_second_stage_name', 'What would you like the second stage to be called?' );
-			echo	go_sub_option('second_stage_button', 'This is the word that will be displayed on the button for the second stage of Task stages. Such as Accept', 'Second Stage Button', 'go_second_stage_button', 'go_second_stage_button', 'What would you like the button for the second stage to say?');
-			echo	go_sub_option( 'third_stage_name', 'This is the word that will be used for the third stage of Task stages. Such as Completed.', 'Third Stage Name', 'go_third_stage_name','go_third_stage_name', 'What would you like the third stage to be called?' );
-			echo	go_sub_option('third_stage_button', 'This is the word that will be displayed on the button for the third stage of Task stages. Such as Complete', 'Third Stage Button', 'go_third_stage_button', 'go_third_stage_button', 'What would you like the button for the third stage to say?');
-			echo	go_sub_option( 'fourth_stage_name', 'This is the word that will be used for the fourth stage of Task stages. Such as Mastered.', 'Fourth Stage Name', 'go_fourth_stage_name','go_fourth_stage_name', 'What would you like the fourth stage to be called?' );
-			echo	go_sub_option('fourth_stage_button', 'This is the word that will be displayed on the button for the fourth stage of Task stages. Such as Master', 'Fourth Stage Button', 'go_fourth_stage_button', 'go_fourth_stage_button', 'What would you like the button for the fourth stage to say?');
-			echo	go_sub_option('repeat_button', 'This is the word that will be displayed on the button for repeat. Such as Repeat.', 'Repeat Button', 'go_repeat_button', 'go_repeat_button', 'What would you like the button for repeat to say?');
+			echo	go_sub_option( 'first_stage_name', 'This is the word that will be used for the first stage of the Task stages which is triggered upon visting the page for the first time. Such as Encountered.', 'First Stage Name', 'go_first_stage_name', 'go_first_stage_name', 'What would you like the first stage to be called?' , 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option( 'second_stage_name', 'This is the word that will be used for the second stage of Task stages. Such as Accepted.', 'Second Stage Name', 'go_second_stage_name','go_second_stage_name', 'What would you like the second stage to be called?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option('second_stage_button', 'This is the word that will be displayed on the button for the second stage of Task stages. Such as Accept', 'Second Stage Button', 'go_second_stage_button', 'go_second_stage_button', 'What would you like the button for the second stage to say?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option( 'third_stage_name', 'This is the word that will be used for the third stage of Task stages. Such as Completed.', 'Third Stage Name', 'go_third_stage_name','go_third_stage_name', 'What would you like the third stage to be called?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option('third_stage_button', 'This is the word that will be displayed on the button for the third stage of Task stages. Such as Complete', 'Third Stage Button', 'go_third_stage_button', 'go_third_stage_button', 'What would you like the button for the third stage to say?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option( 'fourth_stage_name', 'This is the word that will be used for the fourth stage of Task stages. Such as Mastered.', 'Fourth Stage Name', 'go_fourth_stage_name','go_fourth_stage_name', 'What would you like the fourth stage to be called?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option('fourth_stage_button', 'This is the word that will be displayed on the button for the fourth stage of Task stages. Such as Master', 'Fourth Stage Button', 'go_fourth_stage_button', 'go_fourth_stage_button', 'What would you like the button for the fourth stage to say?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
+			echo	go_sub_option('repeat_button', 'This is the word that will be displayed on the button for repeat. Such as Repeat.', 'Repeat Button', 'go_repeat_button', 'go_repeat_button', 'What would you like the button for repeat to say?', 'http://maclab.guhsd.net/go/video/stageNames.m4v');
 
 ?></div>
             <br />
             <div id="curr" class="opt-box">
             <h3>Currency Settings</h3>
             <?php 
-           echo  go_sub_option('currency_name', 'This is what your currency will be called. Use a name like Dollars, or Gold.','Currency Name','go_currency_name', 'go_currency_name', 'what would you like currency to be called?' ); 
-			echo go_sub_option( 'tasks_currency_prefix', 'The prefix symbol used to represent your currency, such as a dollar sign.', 'Currency Prefix', 'go_currency_prefix', 'go_currency_prefix', 'what prefix would you like associated with currency? (Optional)' ); 
-			echo go_sub_option( 'tasks_currency_suffix', 'The suffix symbol used to represent your currency, such as Dollar.', 'Currency Suffix', 'go_currency_suffix', 'go_currency_suffix', 'what suffix would you like associated with currency? (Optional)' ); 
+           echo  go_sub_option('currency_name', 'This is what your currency will be called. Use a name like Dollars, or Gold.','Currency Name','go_currency_name', 'go_currency_name', 'what would you like currency to be called?', 'http://maclab.guhsd.net/go/video/currency.m4v'); 
+			echo go_sub_option( 'tasks_currency_prefix', 'The prefix symbol used to represent your currency, such as a dollar sign.', 'Currency Prefix', 'go_currency_prefix', 'go_currency_prefix', 'what prefix would you like associated with currency? (Optional)', 'http://maclab.guhsd.net/go/video/currency.m4v'); 
+			echo go_sub_option( 'tasks_currency_suffix', 'The suffix symbol used to represent your currency, such as Dollar.', 'Currency Suffix', 'go_currency_suffix', 'go_currency_suffix', 'what suffix would you like associated with currency? (Optional)', 'http://maclab.guhsd.net/go/video/currency.m4v' ); 
             ?>
         </div><br />
             <br />
             <div id="poi" class="opt-box">       
             <h3>Points Settings</h3>
-           <?php echo go_sub_option('tasks_points_name', 'This is what your points will be called. Use a name like Points, or Experience.', 'Points Name', 'go_points_name', 'go_points_name', 'what would you like points to be called?');
-          echo  go_sub_option( 'tasks_points_prefix', 'The prefix symbol used to represent your points, such as a dollar sign.', 'Points Prefix', 'go_points_sym', 'go_points_prefix', 'what prefix would you like associated with points? (Optional)' ); 
-			echo go_sub_option( 'tasks_points_suffix', 'The suffix symbol used to represent your points, such as Exp.', 'Points Suffix', 'go_points_suffix', 'go_points_suffix', 'what suffix would you like associated with points? (Optional)' );  ?>
+           <?php echo go_sub_option('tasks_points_name', 'This is what your points will be called. Use a name like Points, or Experience.', 'Points Name', 'go_points_name', 'go_points_name', 'what would you like points to be called?', 'http://maclab.guhsd.net/go/video/points.m4v');
+          echo  go_sub_option( 'tasks_points_prefix', 'The prefix symbol used to represent your points, such as a dollar sign.', 'Points Prefix', 'go_points_sym', 'go_points_prefix', 'what prefix would you like associated with points? (Optional)', 'http://maclab.guhsd.net/go/video/points.m4v' ); 
+			echo go_sub_option( 'tasks_points_suffix', 'The suffix symbol used to represent your points, such as Exp.', 'Points Suffix', 'go_points_suffix', 'go_points_suffix', 'what suffix would you like associated with points? (Optional)', 'http://maclab.guhsd.net/go/video/points.m4v' );  ?>
           
           
             </div>
@@ -87,13 +94,13 @@ window.location = "'.admin_url().'/?page=game-on-options.php"
             <h3> Admin Bar Settings</h3>
        
           <?php
-		 echo go_sub_option_radio( 'admin_bar_add_trigger', 'Turn on and off the add section of the admin bar.','Add Switch', 'go_admin_bar_add_switch','go_admin_bar_add_switch', 'Would you like to have the Add section of the admin bar?');
+		 echo go_sub_option_radio( 'admin_bar_add_trigger', 'Turn on and off the add section of the admin bar.','Add Switch', 'go_admin_bar_add_switch','go_admin_bar_add_switch', 'Would you like to have the Add section of the admin bar?', 'http://maclab.guhsd.net/go/video/addBar.m4v');
 		   ?><br />
 <div class="opt-box">
             <h3> Infraction Settings</h3>
             <?php
-            echo go_sub_option('max_infractions','When a user has this many infractions they will have 0 life/hitpoints left.','Maximum Infractions','go_max_infractions','go_max_infractions','How many infractions do you want to allow your users?');
-            echo go_sub_option('infractions_name','The name for infractions','Infractions Name','go_infractions_name','go_infractions_name','The name for infractions.');
+            echo go_sub_option('max_infractions','When a user has this many infractions they will have 0 life/hitpoints left.','Maximum Infractions','go_max_infractions','go_max_infractions','How many infractions do you want to allow your users?', 'http://maclab.guhsd.net/go/video/infractions.m4v');
+            echo go_sub_option('infractions_name','The name for infractions','Infractions Name','go_infractions_name','go_infractions_name','The name for infractions.', 'http://maclab.guhsd.net/go/video/infractions.m4v');
                   ?>
             </div>
             </div>
@@ -101,11 +108,11 @@ window.location = "'.admin_url().'/?page=game-on-options.php"
             <h3>Minutes</h3>
        
           <?php
-		echo go_sub_option( 'minutes_bar_color', 'The intervals for the minutes colors.', 'Time', 'go_minutes_color_limit', 'go_minutes_color_limit', '' );
+		echo go_sub_option( 'minutes_bar_color', 'The intervals for the minutes colors.', 'Time', 'go_minutes_color_limit', 'go_minutes_color_limit', '', 'http://maclab.guhsd.net/go/video/time.m4v' );
 		
 		?>
 	    <div class="pa">
-            	<?php go_opt_help('minutes_multi','It adds an extra percentage of points and currency to points and currency gained from tasks.'); ?> 
+            	<?php go_opt_help('minutes_multi','It adds an extra percentage of points and currency to points and currency gained from tasks.', 'http://maclab.guhsd.net/go/video/time.m4v'); ?> 
             	<strong><?php echo 'Percentage Multiplier. Format: percentage, lower limit, upper limit.'; ?>:</strong><br />   
                 On:<input type="radio" <?php if(go_return_options('go_multiplier_switch') == 'On'){echo 'checked="checked"';} ?> name="go_multiplier_switch" size="45" value="On" style="margin-left: 5px;
 width: 20px;" /><br />
@@ -143,8 +150,8 @@ foreach($limit as $key=>$value){
                    <div class="opt-box">       
             <h3> Classifications </h3> 
     <?php
-		 echo go_sub_option( 'class_a_name', 'The name of the first classification. Such as Period or Color.','Classification A Name', 'go_class_a_name','go_class_a_name', 'What would you like to call the first classification?');
-		  echo go_sub_option( 'class_b_name', 'The name of the second classification. Such as Computer or Skill.','Classification B Name', 'go_class_b_name','go_class_b_name', 'What would you like to call the second classification?');
+		 echo go_sub_option( 'class_a_name', 'The name of the first classification. Such as Period or Color.','Classification A Name', 'go_class_a_name','go_class_a_name', 'What would you like to call the first classification?', 'http://maclab.guhsd.net/go/video/classifications.m4v');
+		  echo go_sub_option( 'class_b_name', 'The name of the second classification. Such as Computer or Skill.','Classification B Name', 'go_class_b_name','go_class_b_name', 'What would you like to call the second classification?', 'http://maclab.guhsd.net/go/video/classifications.m4v');
 		   ?>  
             
            </div>
@@ -190,13 +197,13 @@ go_jquery_periods();
         </div>
 
 	<div class="opt-box">
-    	<h3> Focuses </h3>
-        <?php echo go_sub_option( 'focus_name', 'The name of focuses such as \'Career\' or \'Pathway\' ', 'Focus Name', 'go_focus_name','go_focus_name', 'What would you like to call focuses?');?>
+    	<h3> Focus </h3>
+        <?php echo go_sub_option( 'focus_name', 'The name of focuses such as \'Career\' or \'Pathway\' ', 'Focus Name', 'go_focus_name','go_focus_name', 'What would you like to call focuses?', 'http://maclab.guhsd.net/go/video/focuses.m4v');?>
     </div>
     
     <div class="opt-box">
       <div class="pa">
-      	<?php go_opt_help('focus_explanation', 'The switch to turn focuses on and off. Focuses are the areas of interest or study that users choose. Focuses can be purchased via store items.'); ?> 
+      	<?php go_opt_help('focus_explanation', 'The switch to turn focuses on and off. Focuses are the areas of interest or study that users choose. Focuses can be purchased via store items.', 'http://maclab.guhsd.net/go/video/focuses.m4v'); ?> 
         <strong><?php echo 'Turn focuses on or off: ';?> </strong><br />
         On:<input type="radio" <?php if(go_return_options('go_focus_switch') == 'On'){echo 'checked="checked"';} ?> name="go_focus_switch" size="45" value="On" style="margin-left: 5px;width: 20px;" /><br />
         Off:<input type="radio" <?php if(go_return_options('go_focus_switch') == 'Off'){echo 'checked="checked"';} ?>name="go_focus_switch" size="45" value="Off" style="margin-left: 5px;width: 20px;" /><br />
@@ -223,6 +230,7 @@ go_jquery_periods();
             <h3> Presets </h3>  </div>
            <div class="opt-box" > 
                   <div class="pa" style="width:46%;">
+                  <?php go_opt_help('presets_explanation', 'Preset values for tasks. Values are comma separated and correspond with their task stages. E.g., 1,2,3,4 assigns 1 exp to stage 1, 2 exp to stage 2, etc.', 'http://maclab.guhsd.net/go/video/presets.m4v');?><br/>
        
        		<ul id="sortable_go_presets">
        <?php
