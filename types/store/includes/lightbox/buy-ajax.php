62e3116e151bc7417e5bbf75ab9eddc96b398470
foresth <foresthoffman@gmail.com>
2014-08-07 18:02:11 -0700

    Store Limit Bug Fix

--- types/store/includes/lightbox/buy-ajax.php
+++ types/store/includes/lightbox/buy-ajax.php
@@ -39,20 +39,20 @@ function go_buy_item() {
 	$penalty = $custom_fields['go_mta_penalty_switch'];
 
 	$store_limit = unserialize($custom_fields['go_mta_store_limit'][0]);
-	$is_limited = $store_limit[0];
+	$is_limited = (bool)$store_limit[0];
 	if ($is_limited) {
 		$limit = (int)$store_limit[1];
 	}
 
 	$store_filter = unserialize($custom_fields['go_mta_store_filter'][0]);
-	$is_filtered = $store_filter[0];
+	$is_filtered = (bool)$store_filter[0];
 	if ($is_filtered) {
 		$req_rank = $store_filter[1];
 		$bonus_filter = $store_filter[2];
 	}
 	
 	$store_exchange = unserialize($custom_fields['go_mta_store_exchange'][0]);
-	$is_exchangeable = $store_exchange[0];
+	$is_exchangeable = (bool)$store_exchange[0];
 	if ($is_exchangeable) {
 		$exchange_currency = $store_exchange[1];
 		$exchange_points = $store_exchange[2];
@@ -62,7 +62,7 @@ function go_buy_item() {
 	$badge_id = $custom_fields['go_mta_badge_id'][0];
 
 	$store_focus = $custom_fields['go_mta_store_focus'][0];
-	$is_focused = $store_focus[0];
+	$is_focused = (bool)$store_focus[0];
 	if ($is_focused) {
 		$item_focus = $store_focus[1];
 	}
