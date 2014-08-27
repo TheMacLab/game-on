c75053149c9c0faad4c42046eee265134de8213b
EzioBallarin <ezio.t.ballarin@gmail.com>
2014-08-26 23:48:17 -0700

    '2.0.2'

--- game-on.php
+++ game-on.php
@@ -4,7 +4,7 @@ Plugin Name: Game-On
 Description: Gamification tools for teachers.
 Authors: Semar Yousif, Vincent Astolfi, Ezio Ballarin, Forest Hoffman, Austin Vuong, Spencer Nussbaum, Isaac Canada
 Author URI: http://maclab.guhsd.net/
-Version: 2.0.1
+Version: 2.0.2
 */
 include('go_datatable.php');
 include('types/types.php');
@@ -182,7 +182,7 @@ function go_user_redirect ($redirect_to, $request, $user) {
 }
 
 function go_admin_head_notification(){
-	if(get_option('go_display_admin_explanation')){
+	if(get_option('go_display_admin_explanation') && current_user_can('manage_options')){
 		echo "<div id='message' class='update-nag' style='font-size: 16px;'>This is a fresh installation of Game On.<br/>Watch <a href='javascript:;'  onclick='go_display_help_video(&quot;http://maclab.guhsd.net/go/video/gameOn.mp4&quot;);' style='display:inline-block;'>this short video</a> for important information.<br/>Or visit the <a href='http://maclab.guhsd.net/game-on' target='_blank'>documentation page</a>.<br/><a href='javascript:;' onclick='go_remove_admin_notification()'>Dismiss messsage</a></div>";
 		echo "<script>
 			function go_remove_admin_notification(){
