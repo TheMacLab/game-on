585f67d426c6df39416cbffd51c0c2682bb9a718
EzioBallarin <ezio.t.ballarin@gmail.com>
2014-08-27 20:24:37 -0700

    2.0.3

--- game-on.php
+++ game-on.php
@@ -4,7 +4,7 @@ Plugin Name: Game-On
 Description: Gamification tools for teachers.
 Authors: Semar Yousif, Vincent Astolfi, Ezio Ballarin, Forest Hoffman, Austin Vuong, Spencer Nussbaum, Isaac Canada
 Author URI: http://maclab.guhsd.net/
-Version: 2.0.2
+Version: 2.0.3
 */
 include('go_datatable.php');
 include('types/types.php');
