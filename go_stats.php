7ed5c64ec3a4b0637291abd1d24484b5b8cc4b19
EzioBallarin <ezio.t.ballarin@gmail.com>
2014-09-28 10:07:50 -0700

    stats page task list URL CSS update
    - URLs now appear in the next stage to where they were submitted (i.e.
    stage 3 has a URL, in the stats page it would appear in stage 4)

--- go_stats.php
+++ go_stats.php
@@ -140,7 +140,7 @@ function go_stats_task_list () {
 					
 					?>
 					<a href='<?php echo ((!empty($stage_url))?$stage_url:"#");?>' class='<?php echo (($is_admin)?"go_stats_task_admin_stage_wrap":"go_stats_task_stage_wrap go_user");?> <?php echo ((!empty($stage_url))?"go_stats_task_stage_url":"");?>' <?php echo ((!empty($stage_url))?'target="_blank"':"");?>>
-					<div task='<?php echo $task->post_id;?>' stage='<?php echo $i;?>' class='go_stats_task_status <?php if($task->status >= $i || $task->count >= 1){echo 'completed';} if($i > $stage_count){echo 'go_stage_does_not_exist';}?> <?php echo (($i <= 4 && $task->count < 1)?((!empty($stage_url))?"stage_url":""):(($i == 5 && $task->count >= 1 && !empty($stage_url))?"stage_url":""));?> <?php echo ((!empty($url_switch[$i]) && $task->status < $i && $task->count < 1)?'future_url':"");?>' <?php if($task->count >=1){echo "count='{$task->count}'"; }?>><?php if($i == 5 && $task->count > 1){echo $task->count;}?></div>
+					<div task='<?php echo $task->post_id;?>' stage='<?php echo $i;?>' class='go_stats_task_status <?php if($task->status >= $i || $task->count >= 1){echo 'completed';} if($i > $stage_count){echo 'go_stage_does_not_exist';}?> <?php echo (($i <= 4 && $task->count < 1)?((!empty($stage_url))?"stage_url":""):(($i == 5 && $task->count >= 1 && !empty($stage_url))?"stage_url":""));?> <?php echo ((!empty($url_switch[$i-1]) && $task->status < $i && $task->count < 1)?'future_url':"");?>' <?php if($task->count >=1){echo "count='{$task->count}'"; }?>><?php if($i == 5 && $task->count > 1){echo $task->count;}?></div>
 					</a>
 					<?php 
 				}
