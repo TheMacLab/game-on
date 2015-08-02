994c36bace3d4231ed80321e82f8453b3d812f91
Forest Hoffman <foresthoffman@gmail.com>
2015-08-01 19:53:13 -0700

    2.5.0

--- game-on.php
+++ game-on.php
@@ -5,7 +5,7 @@ Plugin URI: http://maclab.guhsd.net/game-on
 Description: Gamification tools for teachers.
 Author: Valhalla Mac Lab
 Author URI: https://github.com/TheMacLab/game-on/blob/master/README.md
-Version: 2.4.9
+Version: 2.5.0
 */
 
 include( 'go_datatable.php' );
