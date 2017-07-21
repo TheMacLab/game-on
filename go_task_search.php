2a6333ee4875d042cf1a5bc96f8f94c70bc3a3b5
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-07-21 12:11:21 -0700

    Fix #138: task edit page search throwing error

--- go_task_search.php
+++ go_task_search.php
@@ -4,7 +4,7 @@ add_filter( 'pre_get_posts', 'filter_search' );
 function filter_search( $query ) {
 
 	// if the post type is null or false, then set it to the default post-type.
-	$post_type = ( ! empty( $_GET['post_type'] ) ? (array) $_GET['post_type'] : array( 'post', 'page' ) );
+	$post_type = ( ! empty( $_GET['post_type'] ) ? $_GET['post_type'] : array( 'post', 'page' ) );
 	
 	// checks if this query is in the main loop (for a post or page) and retrieves relative tasks.
 	if ( $query->is_main_query() ) {
