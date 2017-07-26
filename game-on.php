6fed0a743ab36c4d7f95b57374d88c1707466357
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-07-26 08:22:45 -0700

    Update version to 3.1.1

--- game-on.php
+++ game-on.php
@@ -5,7 +5,7 @@ Plugin URI: http://maclab.guhsd.net/game-on
 Description: Gamification tools for teachers.
 Author: Valhalla Mac Lab
 Author URI: https://github.com/TheMacLab/game-on/blob/master/README.md
-Version: 3.1.0
+Version: 3.1.1
 */
 
 include( 'go_datatable.php' );
