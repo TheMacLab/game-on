f2580f4a41b2e0d0a5efdf13b7298b0b54a2f8f8
Mick McMurray <mcmick619@gmail.com>
2018-08-12 18:13:09 -0700

    More video stuff

--- js/front/scripts/go_videos_fit_and_box.js
+++ js/front/scripts/go_videos_fit_and_box.js
@@ -69,7 +69,7 @@ function Max_width_and_LightboxNow(){
             //alert (lightbox_switch);
 			//add a featherlight lightroom wrapper to the fitvids iframes
 			jQuery(".max-width-video-wrapper:not(.wrapped):has(iframe)").each(function(){
-				jQuery(this).prepend('<a style="display:block;" class="featherlight_wrapper_iframe" href="#" data-featherlight="iframe" ><span style="position:absolute; width:100%; height:100%; top:0; left: 0; z-index: 1;"></span></a>');
+				jQuery(this).prepend('<a style="display:block;" class="featherlight_wrapper_iframe" href="#" ><span style="position:absolute; width:100%; height:100%; top:0; left: 0; z-index: 1;"></span></a>');
 				jQuery(this).addClass('wrapped');
 
 			});
@@ -77,15 +77,34 @@ function Max_width_and_LightboxNow(){
             //adds a html link to the wrapper for featherlight lightbox
 
             jQuery('[class^="featherlight_wrapper_iframe"]').each(function(){
-                var _src = jQuery(this).parent().find('iframe').attr('src');
-                jQuery(this).attr("href", _src);
-                var _href = jQuery(this).attr("href");
-                jQuery(this).attr("href", _href + '?&autoplay=1');
+                var _src = jQuery(this).parent().find('.fluid-width-video-wrapper').parent().html();
+
+                console.log("src2:" + _src);
+                //_src="<div class=\"fluid-width-video-wrapper fit\" style=\"padding-top: 56.1905%;\"><iframe src=\"https://www.youtube.com/embed/zRvOnnoYhKw?feature=oembed?&autoplay=1\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen=\"\" name=\"fitvid0\"></iframe></div>"
+                jQuery(this).attr("href", "<div id=\"go_video_container\" style=\"height: 90vh; overflow: hidden;\">" + _src + "</div>");
+                //var _href = jQuery(this).attr("data-featherlight");
+                //jQuery(this).attr("href", _href + '?&autoplay=1');
                 //activates the lightbox
                 //jQuery.featherlight.defaults.closeOnClick = true;
-                jQuery.featherlight.defaults.iframeWidth = '100%';
-                jQuery.featherlight.defaults.iframeHeight = '100%';
-                jQuery(this).featherlight();
+                //jQuery.featherlight.defaults.iframeWidth = '100%';
+                //jQuery.featherlight.defaults.iframeHeight = '100%';
+                //jQuery(this).featherlight();
+                jQuery('.featherlight_wrapper_iframe').featherlight({
+                    targetAttr: 'href',
+                    closeOnEsc: true,
+                    afterOpen: function(event){
+                        jQuery(".featherlight-content").css({
+							'width' : '90%',
+                            'overflow' : 'hidden'
+                        });
+                        jQuery(".featherlight-content iframe").css({
+                            'height' : '86vh'
+                        });
+                        jQuery(".featherlight-content iframe")[0].src += "&autoplay=1";
+                        ev.preventDefault();
+
+                    }
+                });
             });
 
 
@@ -93,7 +112,7 @@ function Max_width_and_LightboxNow(){
             jQuery(".max-width-video-wrapper:not(.wrapped):has(video)").each(function(){
                 //jQuery(this).prepend('<a style="display:block;" class="featherlight_wrapper_native_vid" href="#" data-featherlight="iframe" ><span style="position:absolute; width:100%; height:100%; top:0; left: 0; z-index: 4;"></span></a>');
 
-                jQuery(this).prepend('<a  class="featherlight_wrapper_vid_native" href="<div id=\'go_video_container\' style=\'height: 90vh;overflow: hidden;\'> <video controls autoplay style=\'height: 100%;\'> <source src=\'#\' type=\'video/mp4\'>Your browser does not support the video tag. </video></div>"<span style=\'position:absolute; width:100%; height:100%; top:0; left: 0; z-index: 4;\'></span></a>');
+                jQuery(this).prepend('<a  class="featherlight_wrapper_vid_native" href="#"><span style=\'position:absolute; width:100%; height:100%; top:0; left: 0; z-index: 4;\'></span></a>');
 
 
                 jQuery(this).addClass('wrapped');
@@ -117,6 +136,7 @@ function Max_width_and_LightboxNow(){
                     //jQuery(this).featherlight();
 					jQuery('.featherlight_wrapper_vid_native').featherlight({
 						targetAttr: 'href',
+                        closeOnEsc: true,
 						afterOpen: function(event){
 							jQuery(".featherlight-content").css("overflow","hidden")
 						}
@@ -124,7 +144,7 @@ function Max_width_and_LightboxNow(){
                 });
             }, 200);
 
-        } 
+        }
  }
 
  function go_test_after(){
