dd653dc8540e085061312d1c07ac887cb02febbd
Mick McMurray <mcmick619@gmail.com>
2018-08-14 20:05:31 -0700

    Clipboard show users before saving profile

--- src/go_clipboard.php
+++ src/go_clipboard.php
@@ -224,7 +224,10 @@ function go_clipboard_intable() {
 
         //$sections = get_user_meta($user_id, "go_sections");
         $num_sections = get_user_meta($user_id, 'go_section_and_seat', true);
-        $sections = array();
+        if (empty($num_sections)){
+            $num_sections =1;
+        }
+        //$sections = array();
 
 
         //$sections = json_encode($sections);
