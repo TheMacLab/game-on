e7e5c394cd71a34cb3157087b4a66a3fe4c597a9
Mick McMurray <mcmick619@gmail.com>
2018-09-04 20:25:22 -0700

    test2

--- src/disabled/go_globals.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * These are global variables that are available
- * via the "global" keyword.
- */
-$go_plugin_dir = dirname( __FILE__ );
-
-?>
--- src/disabled/go_message.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function go_message( $the_message ) {
-	echo wpautop( $the_message, false );
-}
-
-?>
