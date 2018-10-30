0103441a2e71ed2613ba22b3e96b4ce7c1c4462d
Mick McMurray <mcmick619@gmail.com>
2018-10-29 21:36:00 -0700

    media search

--- js/min/go_every_admin_page-min.js
+++ js/min/go_every_admin_page-min.js
@@ -1,4 +1,4 @@
 /*
  * Disable submit with enter key, tab to next field instead
 */
-jQuery(document).ready(function(){jQuery("input,select").bind("keydown",function(e){var t;13===(e.keyCode||e.which)&&(e.preventDefault(),jQuery("input, select, textarea")[jQuery("input,select,textarea").index(this)+1].focus())})});
+jQuery(document).ready(function(){jQuery(".acf-input input,select").bind("keydown",function(e){var t;13===(e.keyCode||e.which)&&(e.preventDefault(),jQuery("input, select, textarea")[jQuery("input,select,textarea").index(this)+1].focus())})});
