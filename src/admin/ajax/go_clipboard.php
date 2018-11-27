e153228225ebe2582eb928ab1cc9c8b6ece11251
Mick McMurray <mcmick619@gmail.com>
2018-11-26 16:11:43 -0800

    error log

--- src/admin/ajax/go_clipboard.php
+++ src/admin/ajax/go_clipboard.php
@@ -1484,6 +1484,7 @@ function go_clipboard_activity() {
 
 function go_clipboard_activity_dataloader_ajax(){
     global $wpdb;
+    $wpdb->show_errors();
 
     //Get the search value
     $search_val = $_GET['search']['value'];
@@ -1750,9 +1751,13 @@ function go_clipboard_activity_dataloader_ajax(){
     ";
     //Add Badge and Group names from the action item?,
     //can't do because they might have multiple saved in a serialized array so it can't be joined.
-    go_write_log($sQuery);
+
+    //go_write_log($sQuery);
     ////columns that will be returned
     $rResult = $wpdb->get_results($sQuery, ARRAY_A);
+
+    go_write_log("ERROR: ");
+    go_write_log($wpdb->print_error());
 
 
     $sQuery2 = "SELECT FOUND_ROWS()";
