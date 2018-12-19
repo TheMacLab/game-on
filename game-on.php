e0a5fc70074136f140be3a12fca884dfa98f011d
Mick McMurray <mcmick619@gmail.com>
2018-12-19 01:12:20 -0800

    4.33

--- game-on.php
+++ game-on.php
@@ -5,7 +5,7 @@ Plugin URI: http://maclab.guhsd.net/game-on
 Description: Gamification tools for teachers.
 Author: Valhalla Mac Lab
 Author URI: https://github.com/TheMacLab/game-on/blob/master/README.md
-Version: 4.32
+Version: 4.33
 */
 
 $go_js_version = 4.32;
