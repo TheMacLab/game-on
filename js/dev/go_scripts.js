221dc82d5acd322b63349cfa96d6dced34cfbc9e
Marquis McMurray <31172935+mcmick@users.noreply.github.com>
2019-03-27 20:56:43 -0700

    myajax

--- js/dev/go_scripts.js
+++ js/dev/go_scripts.js
@@ -1,7 +1,7 @@
-// @codekit-prepend '/scripts/go_every_page.js'
-// @codekit-prepend '/scripts/buy_the_item.js'
-// @codekit-prepend '/scripts/go_fitvids.js'
-// @codekit-prepend '/scripts/go_messages.js'
-// @codekit-prepend '/scripts/go_videos_fit_and_box.js'
-// @codekit-prepend '/scripts/go_map.js'
+// @codekit-prepend 'scripts/go_every_page.js'
+// @codekit-prepend 'scripts/buy_the_item.js'
+// @codekit-prepend 'scripts/go_fitvids.js'
+// @codekit-prepend 'scripts/go_messages.js'
+// @codekit-prepend 'scripts/go_videos_fit_and_box.js'
+// @codekit-prepend 'scripts/go_map.js'
 
